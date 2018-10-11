694e3d7a1c14a111a727f50d1b79d6885cde09f9
Jacob Tomlinson <jacob.tomlinson21@gmail.com>
2018-10-11 15:35:27 -0600

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,41 +2,49 @@ import React, { Component } from 'react';
 
 import '../Home.css';
 
+import coffeeHouse from '../images/coffee-house.jpg';
+
 class About extends Component {
   render() {
     return (
-      <div>
+      <div id='about'>
         <div className="container topper">
-          <h1 className="highlight-blue">About</h1>
+          <h1 className="highlight-blue"><span className='fas fa-angle-double-right'></span> About</h1>
         </div>
-        <div className="container topper">
+        <div className="container">
           <div className="row">
             <div className="col-md-8">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>
+                A coffeehouse, coffee shop, or café is an establishment that primarily serves coffee, related coffee drinks (latte, cappuccino, espresso), and – depending on country – other drinks including alcoholic. Some coffeehouses may serve cold drinks such as iced coffee and iced tea; in continental Europe, cafés serve alcoholic drinks. A coffeehouse may also serve some type of food, such as light snacks, muffins or pastries. Coffeehouses range from owner-operated small businesses to large multinational corporations.
+              </p>
+              <p>
+                From a cultural standpoint, coffeehouses largely serve as centers of social interaction: the coffeehouse provides patrons with a place to congregate, talk, read, write, entertain one another, or pass the time, whether individually or in small groups. Since the development of Wi-Fi, coffeehouses with this capability have also become places for patrons to access the Internet on their laptops and tablet computers. A coffeehouse can serve as an informal club for its regular members. As early as the 1950s Beatnik era and the 1960s folk music scene, coffeehouses have hosted singer-songwriter performances, typically in the evening.
+              </p>
+              <a href='https://en.wikipedia.org/wiki/Coffeehouse' target='_blank' rel='noopener noreferrer'>Source and More Info!</a>
             </div>
             <div className="col-md-4">
-                <p className="placeholder center">This is a placeholder for an image</p>
+                <img src={coffeeHouse} className='img-responsive' alt='coffee house' />
             </div>
           </div>
         </div>
+
         <div className="container-fluid contact-footer">
           <div className="container">
             <h3>Contact us!</h3>
             <form>
-              <div class="form-group">
-                <label for="FormControlInput1">Name</label>
-                <input type="email" class="form-control" id="FormControlInput1" placeholder="John Doe" />
+              <div className="form-group">
+                <label htmlFor="FormControlInput1">Name</label>
+                <input type="email" className="form-control" id="FormControlInput1" placeholder="John Doe" />
               </div>
-              <div class="form-group">
-                <label for="FormControlInput2">Email address</label>
-                <input type="email" class="form-control" id="FormControlInput2" placeholder="name@example.com" />
+              <div className="form-group">
+                <label htmlFor="FormControlInput2">Email address</label>
+                <input type="email" className="form-control" id="FormControlInput2" placeholder="name@example.com" />
               </div>
-              <div class="form-group">
-                <label for="FormControlTextarea1">How can we help?</label>
-                <textarea class="form-control" id="FormControlTextarea1" rows="3"></textarea>
+              <div className="form-group">
+                <label htmlFor="FormControlTextarea1">How can we help?</label>
+                <textarea className="form-control" id="FormControlTextarea1" rows="3"></textarea>
               </div>
-              <button type="submit" class="btn btn-primary mb-2">Submit</button>
+              <button type="submit" className="btn btn-primary mb-2">Submit</button>
             </form>
           </div>
         </div>
